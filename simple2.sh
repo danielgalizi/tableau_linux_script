ca621584db1a9855e8a1a753db0e7792e65930e7
Daniel Galizi <daniel.galizi@gmail.com>
2018-02-09 12:26:02 -0200

    Update simple2.sh

--- simple2.sh
+++ simple2.sh
@@ -19,14 +19,3 @@ select opt in "${options[@]}" "Quit"; do
 
 done
 
-while opt=$(zenity --title="$title" --text="$prompt" --list \
-                   --column="Options" "${options[@]}"); do
-
-    case "$opt" in
-    "${options[0]}" ) zenity --info --text="You picked $opt, option 1";;
-    "${options[1]}" ) zenity --info --text="You picked $opt, option 2";;
-    "${options[2]}" ) zenity --info --text="You picked $opt, option 3";;
-    *) zenity --error --text="Invalid option. Try another one.";;
-    esac
-
-done
